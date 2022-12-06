60c5bf1e85dc3715df2301cb91d7fe901347697e
sarikadevops <sarikadhaygude2@gmail.com>
2022-12-06 11:14:07 +0530

    pom file changes

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class staticapplicationsecuritytesting {
+    public static void main(String[] args) { staticapplicationsecuritytesting.run(github-cicd) }
+    {
+        System.out.println("Hello world!");
+    }
+}
